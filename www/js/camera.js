56c3a27dc4992eb3f9fe87b0b2331b36bd0fc5aa
carlgarpel <carlgarpel@gmail.com>
2017-01-13 14:26:28 +0100

    Add files via upload

---
+++ www/js/camera.js
@@ -0,0 +1,44 @@
+var app = {
+	inicio: function() {
+		this.iniciaFastClick();
+		this.iniciaBoton();
+	},
+
+	iniciaFastClick: function () {
+		FastClick.attach (document.body);
+	},
+
+
+	iniciaBoton: function() {
+	var	buttonAction = document.querySelector('#button-action');
+	buttonAction.addEventListener('click', this.tomarFoto);
+	},
+
+	tomarFoto: function() {
+	var opciones = {
+		quality:50,
+		destinationType: Camera.DestinationType.FILE_URI,
+		targetWidth: 300,
+		targetHeight: 300,
+		correcOrientation: true
+	};
+		navigator.camera.getPicture(app.fotoTomada, app.errorAlTomarFoto, opciones);
+	},
+
+	fotoTomada: function(imageURI) {
+	var image = document.querySelector('#foto');
+	image.src= imageURI;
+	var salida= document.querySelector('.footer');
+	salida.innerHTML ="hola";
+	},
+
+	errorAlTomarFoto: function(message){
+	console.log ("Fallo al tomar foto o toma cancelada: " + message);
+	}
+};
+if ('addEventListener' in document) {
+	document.addEventListener('DOMContentLoaded', function() {
+		app.inicio();
+	}, false);
+}
+
